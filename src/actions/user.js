bee234dae9bd5d75e874ec1e503bb3699caeec6a
Nara Malia <devnaramalia@gmail.com>
2020-04-05 14:25:59 -0600

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,14 +8,93 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: 'purchase 1',
-                amount: 8.02
+                total: 19.40,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
             },
             {
                 _id: 1,
-                title: 'purchase 2',
-                amount: 14.00
+                total: 10.02,
+                orderNumber: 'A0048248347',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 2,
+                total: 5.90,
+                orderNumber: 'A0048248350',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 3,
+                total: 19.00,
+                orderNumber: 'A0048248352',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '4321 West State Street'
+                }
+            },
+            {
+                _id: 4,
+                total: 9.49,
+                orderNumber: 'A0048248362',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 5,
+                total: 1.40,
+                orderNumber: 'A0048248341',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 6,
+                total: 11.50,
+                orderNumber: 'A0048248340',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 7,
+                total: 17.70,
+                orderNumber: 'A0048248349',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
             }
+            
         ]
     })
 }
