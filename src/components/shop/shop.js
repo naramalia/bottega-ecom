5093476c1ece543d99c88d6f73c5691275711d37
Nara Malia <devnaramalia@gmail.com>
2020-04-16 06:47:59 -0600

    filtered products feature complete

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -16,6 +16,7 @@ class Shop extends Component {
         this.props.setHeaderLinks(headerLinks);
         this.props.fetchShopCategories();
         this.props.fetchShopProducts();
+        // this.props.filterProductsWithCategoryId();
             //filter products with links
 
     }
@@ -31,18 +32,34 @@ class Shop extends Component {
         
         return (
             <div className='shop'>
-                {/* shop search bar
-                shop product
-                shop cart button */}
+                {/* shop search bar */}
+                <div className='shop__products'>
+                    {
+                        this.props.filteredProducts.map(product => {
+                            return (
+                                <div key={product._id} className='shop-product'>
+                                    <div className='shop-product__title'> 
+                                        {product.title}
+                                    </div>
+                                    <div className='shop-product__description'>
+                                        {product.description}
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                {/* shop cart button */}
             </div>
         ) 
     }
 }
 
 function mapStateToProps(state) {
-    const { categories } = state.shop;
+    const { categories, filteredProducts } = state.shop;
     return {
-        categories
+        categories,
+        filteredProducts
     }
 }
 
--- src/reducers/shopReducer.js
+++ src/reducers/shopReducer.js
@@ -7,8 +7,8 @@ import {
 
 const INITIAL_STATE = {
     categories: [],
-    productsSelected: [],
-    products: []
+    products: [],
+    filteredProducts: []
 }
 
 export default function(state= INITIAL_STATE, action) {
@@ -25,9 +25,15 @@ export default function(state= INITIAL_STATE, action) {
                 products: action.payload
             }
         case FILTER_PRODUCTS_WITH_CATEGORY_ID:
-            console.log(action.payload);
+            var filteredProducts = [];
+            state.products.map(product => {
+                if(product.belongsTo.includes(action.payload)) {
+                    filteredProducts.push(product);
+                }
+            })
             return {
-                ...state
+                ...state,
+                filteredProducts
             }
         default: return state;
     }
