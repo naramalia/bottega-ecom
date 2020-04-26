7cab269080414b8b613e9754442bd4cc6b94699f
Nara Malia <devnaramalia@gmail.com>
2020-04-26 09:49:53 -0600

    cart animations and cart button in shop component

---
+++ src/components/shop/cartButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function CartButton({className, icon, onClick}) {
+    return (
+        <a onClick={onClick} className={`${className} cart-button`}>
+            <i className={icon}/>
+        </a>
+    )
+}
+
+export default CartButton;
